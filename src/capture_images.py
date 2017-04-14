588f3325463285b531dc16d254b81288257d016b
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2017-04-14 12:59:44 -0500

    added failed point removal and kinect image capture code

--- src/capture_images.py
+++ src/capture_images.py
@@ -19,10 +19,12 @@ import tf_conversions
 from arm_moveit import *
 from utils import *
 import random
+import json
 
 class CaptureImages:
 
   def __init__(self):
+    #rospy.wait_for_service('image_view/save')
     topic = 'visualization_marker_array'
     self.publisher = rospy.Publisher(topic, MarkerArray)
     rospy.sleep(2)
@@ -33,7 +35,9 @@ class CaptureImages:
     self.file_name=""
     self.bounding_box_scale = [0.3,0.6,0.75]
     self.bounding_box_coordinates = [0.9,-0.3,0.65] #relative to base link
-    self.num_points = [2,3,3]
+    self.num_points = [1,2,2]
+    self.failed_points_new = []
+    self.failed_points_old = []
 
   def publish_point(self, pose,color):
     self.markerArray = generate_point(self.markerArray,pose,color)
@@ -68,7 +72,7 @@ class CaptureImages:
   def log(self,pose,id,status):
       if (status):
         with open(self.file_name, 'a+') as f:
-          f.write("\nPicture %i"%id+"\nPosition\n"+str(pose.pose.position)+"\nOrientation\n"+str(pose.pose.orientation)+"\n")
+          f.write("\nSuccess Picture %i"%id+"\nPosition\n"+str(pose.pose.position)+"\nOrientation\n"+str(pose.pose.orientation)+"\n")
       else:
         with open(self.file_name, 'a+') as f:
           f.write("\nFailed Picture %i"%id+"\nPosition\n"+str(pose.pose.position)+"\nOrientation\n"+str(pose.pose.orientation)+"\n")
@@ -77,13 +81,14 @@ class CaptureImages:
 
   def calc_orientation(self,angle,tilt_angle,rotation):
     quaternion = tf.transformations.quaternion_from_euler(radians(angle),radians(-90), 0)
-    quaternion2 = tf.transformations.quaternion_from_euler(0,radians(rotation), 0)
+    quaternion2 = tf.transformations.quaternion_from_euler(0,0, radians(rotation))
     quaternion = tf.transformations.quaternion_multiply(quaternion,tf.transformations.quaternion_inverse(quaternion2))
     return quaternion
 
 
   def execute_movement(self):
     counter = 0
+
     self.publish_box()
     quaternion = tf.transformations.quaternion_from_euler(0,radians(-90),0)
     z = self.bounding_box_coordinates[2]
@@ -93,34 +98,40 @@ class CaptureImages:
       while(x<(self.bounding_box_coordinates[0]+self.bounding_box_scale[0])+0.05):
         y = self.bounding_box_coordinates[1]
         while(y<(self.bounding_box_coordinates[1]+self.bounding_box_scale[1])+0.05):
-          counter+=1
+          
           for rotation in range(-20,21,20):
-          # for tilt_angle in range(0,1,10): 
-            for angle in range(-90,0,45):
-              quaternion = self.calc_orientation(angle,0,rotation)
-              pose = geometry_msgs.msg.Pose()
-              pose.position.x  = x
-              pose.position.y = y
-              pose.position.z = z
-              pose.orientation.x = quaternion[0]
-              pose.orientation.y = quaternion[1]
-              pose.orientation.z = quaternion[2]
-              pose.orientation.w = quaternion[3]
+          #for rotation in range(0,1,20):
+            for angle in range(-45,46,45):
+            #for angle in range(0,1,45):
+              if(counter not in self.failed_points_old):
 
-              ik = self.arm.get_IK(pose)
-              plan = self.arm.plan_jointTargetInput(ik)
+                quaternion = self.calc_orientation(angle,0,rotation)
+                pose = geometry_msgs.msg.Pose()
+                pose.position.x  = x
+                pose.position.y = y
+                pose.position.z = z
+                pose.orientation.x = quaternion[0]
+                pose.orientation.y = quaternion[1]
+                pose.orientation.z = quaternion[2]
+                pose.orientation.w = quaternion[3]
 
-              if(plan!=None):
-                print "executing pose"
-                print pose
-                self.publish_point(pose,[0,1,0])
-                self.capture_position(counter,True)
-                self.arm.group[0].execute(plan)
-              else:
-                self.capture_position(counter,False)
-                self.publish_point(pose,[1,0,0])
-                print "failed pose"
-                print pose
+                ik = self.arm.get_IK(pose)
+                plan = self.arm.plan_jointTargetInput(ik)
+
+                if(plan!=None):
+                  print "executing pose"
+                  print pose
+                  self.publish_point(pose,[0,1,0])
+                  self.capture_position(counter,True)
+                  #self.arm.group[0].execute(plan)
+                else:
+                  self.capture_position(counter,False)
+                  self.publish_point(pose,[1,0,0])
+                  self.failed_points_new.append(counter)
+                  print "failed pose"
+                  print pose
+
+              counter+=1
               
           y+=(self.bounding_box_scale[1]/self.num_points[1])
         x+=(self.bounding_box_scale[0]/self.num_points[0])
@@ -130,7 +141,24 @@ class CaptureImages:
   def capture_position(self,id,status):
 
     self.log(self.arm.group[0].get_current_pose(),id,status)
-      
+
+  def get_failed(self):
+    with open('failed_points.json') as json_data:
+      self.failed_points_old = json.load(json_data)
+
+  def dump_failed(self):
+    
+    with open('failed_points.json', 'w') as outfile:
+      json.dump(self.failed_points_new, outfile)
+    print self.failed_points_new
+
+  def capture_image(self):
+    try:
+      image = rospy.ServiceProxy('image_view/save', AddTwoInts)
+      resp1 = image()
+      return resp1
+    except rospy.ServiceException, e:
+      print "Service call failed: %s"%e
 
 
 def main():
@@ -155,7 +183,9 @@ def main():
     with open(capture_img.file_name, 'w+') as f:
       pass
     if not rospy.is_shutdown():
+      capture_img.get_failed()
       capture_img.execute_movement()
+      #capture_img.dump_failed()
       #capture_img.capture_position()
 
     
